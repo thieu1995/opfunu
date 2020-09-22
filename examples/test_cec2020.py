a66e00ec620bcdd97c2f4651293e5fc25fd528fb
Thieu Nguyen <nguyenthieu2102@gmail.com>
2020-09-22 11:20:05 +0700

    Update test case for cec 2020

--- examples/test_cec2020.py
+++ examples/test_cec2020.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 14:37, 20/09/2020                                                        %
+# Created by "Thieu Nguyen" at 11:19, 22/09/2020                                                        %
 #                                                                                                       %
 #       Email:      nguyenthieu2102@gmail.com                                                           %
 #       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
 #       Github:     https://github.com/thieunguyen5991                                                  %
-#-------------------------------------------------------------------------------------------------------%
+# -------------------------------------------------------------------------------------------------------%
 
 from opfunu.cec.cec2020.constant import benchmark_function as BF
 from opfunu.cec.cec2020 import engineering
 from numpy.random import uniform
 
 for i in range(1, 26):
-    out = BF(i)         # Get object contain information about problems
+    out = BF(i)  # Get object contain information about problems
     D, g, h, xmin, xmax = out["D"], out["g"], out["h"], out["xmin"], out["xmax"]
 
-    solution = uniform(xmin, xmax)                              ## Create solution based on information above
-    problem = "p" + str(i)                                      ## Choice the problem
-    fx, gx, hx = getattr(engineering, problem)(solution)        ## Fitness function, constraint
+    solution = uniform(xmin, xmax)  ## Create solution based on information above
+    problem = "p" + str(i)  ## Choice the problem
+    fx, gx, hx = getattr(engineering, problem)(solution)  ## Fitness function, constraint
     print("\n==============" + problem + "=================")
     print("fx:", fx)
     print("gx:", gx)
     print("hx:", hx)
+
