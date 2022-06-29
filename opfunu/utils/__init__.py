83f97d38c46601205b565a7dbc79a42bb5de639b
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-30 01:11:14 +0700

    Add utilities package

---
+++ opfunu/utils/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 20:46, 29/06/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from .visualize import *
---
+++ opfunu/utils/visualize.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 20:46, 29/06/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from PIL import Image
+import requests
+from io import BytesIO
+
+
+cmap = [(0, '#2f9599'), (0.45, '#eeeeee'), (1, '#8800ff')]
+cmap = cm.colors.LinearSegmentedColormap.from_list('Custom', cmap, N=256)
+
+
+def plot_latex_formula(latex):
+    base_url = r'https://latex.codecogs.com/png.latex?\dpi{400}\\'
+    url = f'{base_url}{latex}'
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+
+    plt.imshow(img)
+    plt.show()
+
+
+def plot_2d(func, n_space=1000, cmap=cmap, XYZ=None, ax=None, show=True):
+    X_domain, Y_domain = func.bounds
+    if XYZ is None:
+        X, Y = np.linspace(*X_domain, n_space), np.linspace(*Y_domain, n_space)
+        X, Y = np.meshgrid(X, Y)
+        XY = np.array([X, Y])
+        Z = np.apply_along_axis(func.evaluate, 0, XY)
+    else:
+        X, Y, Z = XYZ
+
+    # create new ax if None
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+    # add contours and contours lines
+    # ax.contour(X, Y, Z, levels=30, linewidths=0.5, colors='#999')
+    ax.contourf(X, Y, Z, levels=30, cmap=cmap, alpha=0.7)
+
+    # add labels and set equal aspect ratio
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_aspect(aspect='equal')
+    if show:
+        plt.show()
+
+
+def plot_3d(func, n_space=1000, cmap=cmap, XYZ=None, ax=None, show=True):
+    X_domain, Y_domain = func.bounds
+    if XYZ is None:
+        X, Y = np.linspace(*X_domain, n_space), np.linspace(*Y_domain, n_space)
+        X, Y = np.meshgrid(X, Y)
+        XY = np.array([X, Y])
+        Z = np.apply_along_axis(func.evaluate, 0, XY)
+    else:
+        X, Y, Z = XYZ
+
+    # create new ax if None
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    # Plot the surface.
+    ax.plot_surface(X, Y, Z, cmap=cmap, linewidth=0, antialiased=True, alpha=0.7)
+    ax.contour(X, Y, Z, zdir='z', levels=30, offset=np.min(Z), cmap=cmap)
+
+    ax.xaxis.pane.fill = False
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.xaxis.set_tick_params(labelsize=8)
+    ax.yaxis.set_tick_params(labelsize=8)
+    ax.zaxis.set_tick_params(labelsize=8)
+    if show:
+        plt.show()
