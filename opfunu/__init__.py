0b8cf26b92b089d3c71775913fb8238e13f6dff9
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-30 12:28:13 +0700

    Add more search functions

--- opfunu/__init__.py
+++ opfunu/__init__.py
@@ -7,14 +7,16 @@
 import inspect
 from .utils import *
 from . import name_based
+from . import cec_based
 
-FUNCS_DATABASE = inspect.getmembers(name_based, inspect.isclass)
-FUNCS_EXCLUDES = ["Benchmark"]
+FUNC_DATABASE = inspect.getmembers(name_based, inspect.isclass)
+CEC_DATABASE = inspect.getmembers(cec_based, inspect.isclass)
+EXCLUDES = ["Benchmark", "CecBenchmark", "ABC"]
 
 
 def get_functions(ndim, continuous=None, linear=None, convex=None, unimodal=None, separable=None,
                   differentiable=None, scalable=None, randomized_term=None, parametric=None, modality=None):
-    functions = [cls for classname, cls in FUNCS_DATABASE if classname not in FUNCS_EXCLUDES]
+    functions = [cls for classname, cls in FUNC_DATABASE if classname not in EXCLUDES]
     functions = list(filter(lambda f: f().is_ndim_compatible(ndim), functions))
 
     functions = list(filter(lambda f: (continuous is None) or (f.continuous == continuous), functions))
@@ -28,3 +30,23 @@ def get_functions(ndim, continuous=None, linear=None, convex=None, unimodal=None
     functions = list(filter(lambda f: (parametric is None) or (f.parametric == parametric), functions))
     functions = list(filter(lambda f: (modality is None) or (f.modality == modality), functions))
     return functions
+
+
+def get_cecs(ndim, continuous=None, linear=None, convex=None, unimodal=None, separable=None, differentiable=None,
+             scalable=None, randomized_term=None, parametric=True, shifted=True, modality=None):
+    functions = [cls for classname, cls in CEC_DATABASE if classname not in EXCLUDES]
+    functions = list(filter(lambda f: f().is_ndim_compatible(ndim), functions))
+
+    functions = list(filter(lambda f: (continuous is None) or (f.continuous == continuous), functions))
+    functions = list(filter(lambda f: (linear is None) or (f.linear == linear), functions))
+    functions = list(filter(lambda f: (convex is None) or (f.convex == convex), functions))
+    functions = list(filter(lambda f: (unimodal is None) or (f.unimodal == unimodal), functions))
+    functions = list(filter(lambda f: (separable is None) or (f.separable == separable), functions))
+    functions = list(filter(lambda f: (differentiable is None) or (f.differentiable == differentiable), functions))
+    functions = list(filter(lambda f: (scalable is None) or (f.scalable == scalable), functions))
+    functions = list(filter(lambda f: (randomized_term is None) or (f.randomized_term == randomized_term), functions))
+    functions = list(filter(lambda f: (parametric is None) or (f.parametric == parametric), functions))
+    functions = list(filter(lambda f: (shifted is None) or (f.shifted == shifted), functions))
+    functions = list(filter(lambda f: (modality is None) or (f.modality == modality), functions))
+    return functions
+
---
+++ opfunu/cec_based/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 06:25, 30/06/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from .cec2005 import *
+
+__all__ = [s for s in dir() if not s.startswith('_')]
