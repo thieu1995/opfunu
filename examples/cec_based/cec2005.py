a41a8f84c914d47dba28dce99d23f3a732160ef2
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-30 21:02:01 +0700

    Add CEC2005 F4

---
+++ examples/cec_based/cec2005.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 20:56, 30/06/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import opfunu
+import numpy as np
+
+## Test CEC2005 F1
+problem = opfunu.cec_based.F12005(ndim=25)
+x = np.ones(25)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2005 F2
+problem = opfunu.cec_based.F22005(ndim=23)
+x = np.ones(23)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2005 F3
+problem = opfunu.cec_based.F32005(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2005 F4
+problem = opfunu.cec_based.F42005(ndim=100)
+x = np.ones(100)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+
+
--- opfunu/cec_based/cec2005.py
+++ opfunu/cec_based/cec2005.py
@@ -50,7 +50,7 @@ class F12005(CecBenchmark):
 
     def evaluate(self, x, *args):
         self.n_fe += 1
-        self.check_solution(x, self.dim_max, None)
+        self.check_solution(x, self.dim_max, self.dim_supported)
         return np.sum((x - self.f_shift) ** 2) + self.f_bias
 
 
@@ -96,7 +96,7 @@ class F22005(CecBenchmark):
 
     def evaluate(self, x, *args):
         self.n_fe += 1
-        self.check_solution(x, self.dim_max, None)
+        self.check_solution(x, self.dim_max, self.dim_supported)
         ndim = len(x)
         results = [np.sum(x[:idx] - self.f_shift[:idx])**2 for idx in range(0, ndim)]
         return np.sum(results) + self.f_bias
@@ -151,3 +151,52 @@ class F32005(CecBenchmark):
         z = (np.dot((x - self.f_shift), self.f_matrix)) ** 2
         results = [(10**6)**(idx / (ndim - 1)) * z[idx]**2 for idx in range(0, ndim)]
         return np.sum(results) + self.f_bias
+
+
+class F42005(CecBenchmark):
+    """
+    .. [1] Suganthan, P.N., Hansen, N., Liang, J.J., Deb, K., Chen, Y.P., Auger, A. and Tiwari, S., 2005.
+    Problem definitions and evaluation criteria for the CEC 2005 special session on real-parameter optimization.
+    KanGAL report, 2005005(2005), p.2005.
+    """
+    name = "F4: Shifted Schwefel’s Problem 1.2 with Noise in Fitness"
+    latex_formula = r'F_4(x) = \Big(\sum_{i=1}^D (\sum_{j=1}^i)^2\Big)*\Big(1 + 0.4|N(0, 1)|\Big)+ bias;\\ z=(x-o).M; x=[x_1, ..., x_D], \\o=[o_1, ..., o_D]: \text{the shifted global optimum}\\ N(0,1): \text{gaussian noise}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r"x_i \in [-100.0, 100.0], \forall i \in [1, D]"
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_4(x^*) = bias = -450.0'
+    continuous = True
+    linear = False
+    convex = True
+    unimodal = True
+    separable = False
+
+    differentiable = True
+    scalable = True
+    randomized_term = True
+    parametric = True
+    shifted = True
+
+    modality = True  # Number of ambiguous peaks, unknown # peaks
+    # n_basins = 1
+    # n_valleys = 1
+
+    def __init__(self, ndim=None, bounds=None, f_shift="data_schwefel_102", f_bias=-450.):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 30
+        self.dim_max = 100
+        self.check_ndim_and_bounds(ndim, self.dim_max, bounds, np.array([[-100., 100.] for _ in range(self.dim_default)]))
+        self.make_support_data_path("data_2005")
+        self.f_shift = self.check_shift_data(f_shift)[:self.ndim]
+        self.f_bias = f_bias
+        self.f_global = f_bias
+        self.x_global = self.f_shift
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+        ndim = len(x)
+        results = [np.sum(x[:idx] - self.f_shift[:idx])**2 for idx in range(0, ndim)]
+        return np.sum(results) * (1 + 0.4 * np.abs(np.random.normal(0, 1))) + self.f_bias
+
