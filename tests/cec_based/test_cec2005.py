1650f4782ce165cbd1f7929fd9bc41f8864351ae
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-01 17:05:41 +0700

    Add CEC2005 F20

--- tests/cec_based/test_cec2005.py
+++ tests/cec_based/test_cec2005.py
@@ -256,6 +256,17 @@ def test_F192005_results():
     assert len(problem.x_global) == ndim
 
 
+def test_F202005_results():
+    ndim = 30
+    problem = opfunu.cec_based.F202005(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
 
 
 
