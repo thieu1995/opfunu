d95629b88e00b9a1ae93ec62c4e50a50f0ace74d
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-01 16:46:13 +0700

    Add CEC2005 F18

--- examples/cec_based/cec2005.py
+++ examples/cec_based/cec2005.py
@@ -213,6 +213,17 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2005 F18
+print("====================F18")
+problem = opfunu.cec_based.F182005(ndim=30)
+x = np.ones(30)
+print(problem.f_global)
+print(problem.evaluate(x))
+print(problem.x_global)
+print(problem.evaluate(problem.x_global))
+
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
 
 
 
