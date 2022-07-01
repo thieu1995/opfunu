32ae3d012331e0688df381021c6ea1988585ddc3
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-01 22:28:38 +0700

    Add CEC-2008 F1

---
+++ examples/cec_based/cec2008.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 22:26, 01/07/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import opfunu
+import numpy as np
+
+## Test CEC2008 F1
+print("====================F1")
+problem = opfunu.cec_based.F12008(ndim=100)
+x = np.ones(100)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
--- opfunu/cec_based/__init__.py
+++ opfunu/cec_based/__init__.py
@@ -5,5 +5,6 @@
 # --------------------------------------------------%
 
 from .cec2005 import *
+from .cec2008 import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
