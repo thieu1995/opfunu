b0d6cf673bb8359a848d3433d63fb00066712d73
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-02 23:04:53 +0700

    Add CEC-2013 F12

--- examples/cec_based/cec2013.py
+++ examples/cec_based/cec2013.py
@@ -139,6 +139,16 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2013 F12
+print("====================F12")
+problem = opfunu.cec_based.F122013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
 
 
 
