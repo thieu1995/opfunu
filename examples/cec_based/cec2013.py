7c61a68e79626192468fb72d83d7b2c691119756
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-03 21:16:46 +0700

    Add CEC-2013 F28

--- examples/cec_based/cec2013.py
+++ examples/cec_based/cec2013.py
@@ -7,244 +7,244 @@
 import opfunu
 import numpy as np
 
-# ## Test CEC2013 F1
-# print("====================F1")
-# problem = opfunu.cec_based.F12013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F2
-# print("====================F2")
-# problem = opfunu.cec_based.F22013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F3
-# print("====================F3")
-# problem = opfunu.cec_based.F32013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F4
-# print("====================F4")
-# problem = opfunu.cec_based.F42013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F5
-# print("====================F5")
-# problem = opfunu.cec_based.F52013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F6
-# print("====================F6")
-# problem = opfunu.cec_based.F62013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F7
-# print("====================F7")
-# problem = opfunu.cec_based.F72013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F8
-# print("====================F8")
-# problem = opfunu.cec_based.F82013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F9
-# print("====================F9")
-# problem = opfunu.cec_based.F92013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F10
-# print("====================F10")
-# problem = opfunu.cec_based.F102013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F11
-# print("====================F11")
-# problem = opfunu.cec_based.F112013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F12
-# print("====================F12")
-# problem = opfunu.cec_based.F122013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F13
-# print("====================F13")
-# problem = opfunu.cec_based.F132013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F14
-# print("====================F14")
-# problem = opfunu.cec_based.F142013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F15
-# print("====================F15")
-# problem = opfunu.cec_based.F152013(ndim=50)
-# x = np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F16
-# print("====================F16")
-# problem = opfunu.cec_based.F162013(ndim=50)
-# x = 90*np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F17
-# print("====================F17")
-# problem = opfunu.cec_based.F172013(ndim=50)
-# x = 90*np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F18
-# print("====================F18")
-# problem = opfunu.cec_based.F182013(ndim=50)
-# x = 90*np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F19
-# print("====================F19")
-# problem = opfunu.cec_based.F192013(ndim=50)
-# x = 90*np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
-#
-#
-# ## Test CEC2013 F20
-# print("====================F20")
-# problem = opfunu.cec_based.F202013(ndim=50)
-# x = 90*np.ones(50)
-# print(problem.evaluate(x))
-# print(problem.x_global)
-#
-# print(problem.evaluate(problem.x_global))
-# print(problem.is_succeed(x))
-# print(problem.is_succeed(problem.x_global))
+## Test CEC2013 F1
+print("====================F1")
+problem = opfunu.cec_based.F12013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F2
+print("====================F2")
+problem = opfunu.cec_based.F22013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F3
+print("====================F3")
+problem = opfunu.cec_based.F32013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F4
+print("====================F4")
+problem = opfunu.cec_based.F42013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F5
+print("====================F5")
+problem = opfunu.cec_based.F52013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F6
+print("====================F6")
+problem = opfunu.cec_based.F62013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F7
+print("====================F7")
+problem = opfunu.cec_based.F72013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F8
+print("====================F8")
+problem = opfunu.cec_based.F82013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F9
+print("====================F9")
+problem = opfunu.cec_based.F92013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F10
+print("====================F10")
+problem = opfunu.cec_based.F102013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F11
+print("====================F11")
+problem = opfunu.cec_based.F112013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F12
+print("====================F12")
+problem = opfunu.cec_based.F122013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F13
+print("====================F13")
+problem = opfunu.cec_based.F132013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F14
+print("====================F14")
+problem = opfunu.cec_based.F142013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F15
+print("====================F15")
+problem = opfunu.cec_based.F152013(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F16
+print("====================F16")
+problem = opfunu.cec_based.F162013(ndim=50)
+x = 90*np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F17
+print("====================F17")
+problem = opfunu.cec_based.F172013(ndim=50)
+x = 90*np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F18
+print("====================F18")
+problem = opfunu.cec_based.F182013(ndim=50)
+x = 90*np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F19
+print("====================F19")
+problem = opfunu.cec_based.F192013(ndim=50)
+x = 90*np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2013 F20
+print("====================F20")
+problem = opfunu.cec_based.F202013(ndim=50)
+x = 90*np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
 
 
 ## Test CEC2013 F21
@@ -331,3 +331,13 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2013 F28
+print("====================F28")
+problem = opfunu.cec_based.F282013(ndim=30)
+x = 10*np.ones(30)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
--- opfunu/cec_based/cec2013.py
+++ opfunu/cec_based/cec2013.py
@@ -161,7 +161,7 @@ class F32013(CecBenchmark):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         z = operator.tasy_func(np.dot(M1, x - self.f_shift), beta=0.5)
         return operator.bent_cigar_func(np.dot(M2, z)) + self.f_bias
 
@@ -264,7 +264,7 @@ class F72013(F32013):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         alpha = operator.generate_diagonal_matrix(self.ndim, alpha=10)
         temp = operator.tasy_func(np.dot(M1, x - self.f_shift), beta=0.5)
         y = np.dot(np.matmul(alpha, M2), temp)
@@ -297,7 +297,7 @@ class F82013(F32013):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         alpha = operator.generate_diagonal_matrix(self.ndim, alpha=10)
         temp = operator.tasy_func(np.dot(M1, x - self.f_shift), beta=0.5)
         z = np.dot(np.matmul(alpha, M2), temp)
@@ -332,7 +332,7 @@ class F92013(F32013):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         alpha = operator.generate_diagonal_matrix(self.ndim, alpha=10)
         temp = operator.tasy_func(np.dot(M1, 0.5*(x - self.f_shift)/100), beta=0.5)
         z = np.dot(np.matmul(alpha, M2), temp)
@@ -424,7 +424,7 @@ class F122013(F32013):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         alpha = operator.generate_diagonal_matrix(self.ndim, alpha=10)
         temp1 = np.matmul(np.matmul(M1, alpha), M2)
         temp2 = operator.tosz_func(np.dot(M1, 5.12*(x - self.f_shift)/100))
@@ -462,7 +462,7 @@ class F132013(F32013):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         alpha = operator.generate_diagonal_matrix(self.ndim, alpha=10)
         x_star = np.dot(M1, 5.12*(x - self.f_shift)/100)
         y = operator.rounder(x_star, np.abs(x_star))
@@ -568,7 +568,7 @@ class F162013(F32013):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         alpha = operator.generate_diagonal_matrix(self.ndim, alpha=100)
         temp = np.dot(M1, 5.*(x - self.f_shift)/100)
         z = np.dot(np.matmul(M2, alpha), temp)
@@ -639,7 +639,7 @@ class F182013(F32013):
         self.check_solution(x, self.dim_max, self.dim_supported)
         miu0 = 2.5
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         alpha = operator.generate_diagonal_matrix(self.ndim, alpha=100)
         y = 10.0*(x - self.f_shift)/100
         x_hat = 2*np.sign(y)*y + miu0
@@ -682,7 +682,7 @@ class F202013(F32013):
     for the CEC 2013 special session on real-parameter optimization. Computational Intelligence Laboratory, Zhengzhou University,
     Zhengzhou, China and Nanyang Technological University, Singapore, Technical Report, 201212(34), 281-295..
     """
-    name = "F3: Rotated Bent Cigar Function"
+    name = "F20: Rotated Expanded Scaffer’s F6 Function"
     latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
     latex_formula_dimension = r'2 <= D <= 100'
     latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
@@ -702,7 +702,7 @@ class F202013(F32013):
         self.n_fe += 1
         self.check_solution(x, self.dim_max, self.dim_supported)
         M1 = self.f_matrix[:self.ndim, :]
-        M2 = self.f_matrix[self.ndim:, :]
+        M2 = self.f_matrix[self.ndim:2*self.ndim, :]
         z = np.dot(M2, operator.tasy_func(np.dot(M1, x - self.f_shift), beta=0.5))
         results = [operator.scaffer_func([z[idx], z[idx+1]]) for idx in range(0, self.ndim-1)]
         return np.sum(results) + operator.scaffer_func([z[-1], z[0]]) + self.f_bias
@@ -1198,4 +1198,85 @@ class F272013(CecBenchmark):
         return np.dot(ws, gs) + self.f_bias
 
 
+class F282013(CecBenchmark):
+    """
+    .. [1] Liang, J. J., Qu, B. Y., Suganthan, P. N., & Hernández-Díaz, A. G. (2013). Problem definitions and evaluation criteria
+    for the CEC 2013 special session on real-parameter optimization. Computational Intelligence Laboratory, Zhengzhou University,
+    Zhengzhou, China and Nanyang Technological University, Singapore, Technical Report, 201212(34), 281-295..
+    """
+    name = "F28: Composition Function 8"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 1400.0'
 
+    continuous = True
+    linear = False
+    convex = False
+    unimodal = False
+    separable = False
+
+    differentiable = True
+    scalable = True
+    randomized_term = False
+    parametric = True
+    shifted = True
+    rotated = True
+
+    modality = True  # Number of ambiguous peaks, unknown # peaks
+    # n_basins = 1
+    # n_valleys = 1
+    characteristics = ["Asymmetrical", "Different properties around different local optima"]
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data", f_matrix="M_D", f_bias=1400.):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 30
+        self.dim_max = 100
+        self.dim_supported = [2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        self.check_ndim_and_bounds(ndim, self.dim_max, bounds, np.array([[-100., 100.] for _ in range(self.dim_default)]))
+        self.make_support_data_path("data_2013")
+        self.f_shift = self.check_shift_matrix(f_shift)[:, :self.ndim]
+        self.f_matrix = self.check_matrix_data(f_matrix)[:, :self.ndim]
+        self.f_bias = f_bias
+        self.f_global = f_bias
+        self.x_global = self.f_shift[0]
+        self.n_funcs = 5
+        self.xichmas = [10, 20, 30, 40, 50]
+        self.lamdas = [2.5, 2.5e-6, 2.5, 5e-4, 0.1]
+        self.bias = [0, 100, 200, 300, 400]
+        self.g0 = F192013(self.ndim, None, self.f_shift[0], self.f_matrix[:self.ndim, :self.ndim], f_bias=0)
+        self.g1 = F72013(self.ndim, None, self.f_shift[1], self.f_matrix[2*self.ndim:4*self.ndim, :self.ndim], f_bias=0)
+        self.g2 = F152013(self.ndim, None, self.f_shift[2], self.f_matrix[:self.ndim, :self.ndim], f_bias=0)
+        self.g3 = F202013(self.ndim, None, self.f_shift[3], self.f_matrix[:2*self.ndim, :self.ndim], f_bias=0)
+        self.g4 = F12013(self.ndim, None, self.f_shift[4], f_bias=0)
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias, "f_matrix": self.f_matrix}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+
+        # g1: Rotated Expanded Griewank’s plus Rosenbrock’s Function f19’
+        g0 = self.lamdas[0] * self.g0.evaluate(x) + self.bias[0]
+        w0 = operator.calculate_weight(x - self.f_shift[0], self.xichmas[0])
+
+        # g2: Rotated Schaffers F7 Function f7’
+        g1 = self.lamdas[1] * self.g1.evaluate(x) + self.bias[1]
+        w1 = operator.calculate_weight(x - self.f_shift[1], self.xichmas[1])
+
+        # g3: Rotated Schwefel's Function f15'
+        g2 = self.lamdas[2] * self.g2.evaluate(x) + self.bias[2]
+        w2 = operator.calculate_weight(x - self.f_shift[2], self.xichmas[2])
+
+        # g4: Rotated Expanded Scaffer’s F6 Function f20’
+        g3 = self.lamdas[3] * self.g3.evaluate(x) + self.bias[3]
+        w3 = operator.calculate_weight(x - self.f_shift[3], self.xichmas[3])
+
+        # g5: Sphere Function f1’
+        g4 = self.lamdas[4] * self.g4.evaluate(x) + self.bias[4]
+        w4 = operator.calculate_weight(x - self.f_shift[4], self.xichmas[4])
+
+        ws = np.array([w0, w1, w2, w3, w4])
+        ws = ws / np.sum(ws)
+        gs = np.array([g0, g1, g2, g3, g4])
+        return np.dot(ws, gs) + self.f_bias
