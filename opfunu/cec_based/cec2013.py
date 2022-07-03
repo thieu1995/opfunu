ae40577cc414239d6f54f54dd5936327e888c31e
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-03 09:56:53 +0700

    Add CEC-2013 F19

--- opfunu/cec_based/cec2013.py
+++ opfunu/cec_based/cec2013.py
@@ -647,3 +647,33 @@ class F182013(F32013):
         return operator.lunacek_bi_rastrigin_func(x_hat, z, miu0=miu0, d=1) + self.f_bias
 
 
+class F192013(F22013):
+    """
+    .. [1] Liang, J. J., Qu, B. Y., Suganthan, P. N., & Hernández-Díaz, A. G. (2013). Problem definitions and evaluation criteria
+    for the CEC 2013 special session on real-parameter optimization. Computational Intelligence Laboratory, Zhengzhou University,
+    Zhengzhou, China and Nanyang Technological University, Singapore, Technical Report, 201212(34), 281-295..
+    """
+    name = "F19: Rotated Expanded Griewank’s plus Rosenbrock’s Function"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 500.0'
+
+    unimodal = False
+    modality = True  # Number of ambiguous peaks, unknown # peaks
+    # n_basins = 1
+    # n_valleys = 1
+    characteristics = []
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data", f_matrix="M_D", f_bias=500.):
+        super().__init__(ndim, bounds, f_shift, f_matrix, f_bias)
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+        z = np.dot(self.f_matrix, 5*(x - self.f_shift)/100) + 1
+        results = [operator.griewank_func(operator.rosenbrock_func([z[idx], z[idx+1]])) for idx in range(0, self.ndim-1)]
+        return np.sum(results) + operator.griewank_func(operator.rosenbrock_func([z[-1], z[0]])) + self.f_bias
+
+
+
