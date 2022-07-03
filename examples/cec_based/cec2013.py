29ac1f4e1ccf85933083ebe61e05d3a7727de2bc
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-03 10:03:14 +0700

    Add CEC-2013 F20

--- examples/cec_based/cec2013.py
+++ examples/cec_based/cec2013.py
@@ -235,4 +235,16 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2013 F20
+print("====================F20")
+problem = opfunu.cec_based.F202013(ndim=50)
+x = 90*np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
 
--- tests/cec_based/test_cec2013.py
+++ tests/cec_based/test_cec2013.py
@@ -256,6 +256,17 @@ def test_F192013_results():
     assert len(problem.x_global) == ndim
 
 
+def test_F202013_results():
+    ndim = 50
+    problem = opfunu.cec_based.F202013(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
 
 
 
