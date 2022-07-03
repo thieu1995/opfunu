db78e48b66c010cd597ec5f820da8b4697896c0c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-03 20:25:51 +0700

    Add CEC-2013 F23

--- examples/cec_based/cec2013.py
+++ examples/cec_based/cec2013.py
@@ -271,3 +271,16 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2013 F23
+print("====================F23")
+problem = opfunu.cec_based.F232013(ndim=30)
+x = 90*np.ones(30)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+
