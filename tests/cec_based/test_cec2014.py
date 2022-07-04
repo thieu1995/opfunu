3a2aecd91a769ed320e81391d3c59b03b6c67365
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-04 17:35:01 +0700

    Add CEC-2014 F11

--- tests/cec_based/test_cec2014.py
+++ tests/cec_based/test_cec2014.py
@@ -139,3 +139,16 @@ def test_F102014_results():
     assert len(problem.x_global) == ndim
 
 
+def test_F112014_results():
+    ndim = 50
+    problem = opfunu.cec_based.F112014(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
+
+
