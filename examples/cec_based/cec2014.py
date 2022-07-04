3264527e10bfdae7e333652e63f0ca944457a0f5
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-04 16:14:46 +0700

    Add CEC-2014 F2

--- examples/cec_based/cec2014.py
+++ examples/cec_based/cec2014.py
@@ -19,3 +19,15 @@ print(problem.evaluate(problem.x_global))
 print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
+
+## Test CEC2014 F2
+print("====================F2")
+problem = opfunu.cec_based.F22014(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
--- opfunu/cec_based/cec2014.py
+++ opfunu/cec_based/cec2014.py
@@ -61,3 +61,51 @@ class F12014(CecBenchmark):
         z = np.dot(self.f_matrix, x - self.f_shift)
         return operator.elliptic_func(z) + self.f_bias
 
+
+class F22014(F12014):
+    """
+    .. [1] Liang, J. J., Qu, B. Y., & Suganthan, P. N. (2013). Problem definitions and evaluation criteria for the CEC 2014
+    special session and competition on single objective real-parameter numerical optimization. Computational Intelligence Laboratory,
+    Zhengzhou University, Zhengzhou China and Technical Report, Nanyang Technological University, Singapore, 635, 490.
+    """
+    name = "F2: Rotated Bent Cigar Function"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 200.0'
+
+    characteristics = ["Smooth but narrow ridge"]
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data_2", f_matrix="M_2_D", f_bias=200.):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 30
+        self.dim_max = 100
+        self.dim_supported = [10, 20, 30, 50, 100]
+        self.check_ndim_and_bounds(ndim, self.dim_max, bounds, np.array([[-100., 100.] for _ in range(self.dim_default)]))
+        self.make_support_data_path("data_2014")
+        self.f_shift = self.check_shift_data(f_shift)[:self.ndim]
+        self.f_matrix = self.check_matrix_data(f_matrix, needed_dim=True)
+        self.f_bias = f_bias
+        self.f_global = f_bias
+        self.x_global = self.f_shift
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias, "f_matrix": self.f_matrix}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+        z = np.dot(self.f_matrix, x - self.f_shift)
+        return operator.bent_cigar_func(z) + self.f_bias
+
+
+
+
+
+
+
+
+
+
+
+
+
