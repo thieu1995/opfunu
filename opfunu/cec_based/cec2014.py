70c96653f35b89ecbf95eb4b25df0fb985bb3d71
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-04 16:31:29 +0700

    Add CEC-2014 F5

--- opfunu/cec_based/cec2014.py
+++ opfunu/cec_based/cec2014.py
@@ -172,7 +172,42 @@ class F42014(F12014):
         return operator.rosenbrock_func(z) + self.f_bias
 
 
+class F52014(F12014):
+    """
+    .. [1] Liang, J. J., Qu, B. Y., & Suganthan, P. N. (2013). Problem definitions and evaluation criteria for the CEC 2014
+    special session and competition on single objective real-parameter numerical optimization. Computational Intelligence Laboratory,
+    Zhengzhou University, Zhengzhou China and Technical Report, Nanyang Technological University, Singapore, 635, 490.
+    """
+    name = "F5: Shifted and Rotated Ackley’s Function"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 500.0'
 
+    unimodal = False
+
+    characteristics = ["Having a very narrow valley from local optimum to global optimum"]
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data_5", f_matrix="M_5_D", f_bias=500.):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 30
+        self.dim_max = 100
+        self.dim_supported = [10, 20, 30, 50, 100]
+        self.check_ndim_and_bounds(ndim, self.dim_max, bounds, np.array([[-100., 100.] for _ in range(self.dim_default)]))
+        self.make_support_data_path("data_2014")
+        self.f_shift = self.check_shift_data(f_shift)[:self.ndim]
+        self.f_matrix = self.check_matrix_data(f_matrix, needed_dim=True)
+        self.f_bias = f_bias
+        self.f_global = f_bias
+        self.x_global = self.f_shift
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias, "f_matrix": self.f_matrix}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+        z = np.dot(self.f_matrix, x - self.f_shift)
+        return operator.ackley_func(z) + self.f_bias
 
 
 
