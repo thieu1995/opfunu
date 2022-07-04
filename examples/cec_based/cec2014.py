6dadc51c6fd0eb5edfe8e04c18842685b5d1e57a
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-04 16:30:40 +0700

    Add CEC-2014 F4

--- examples/cec_based/cec2014.py
+++ examples/cec_based/cec2014.py
@@ -42,3 +42,17 @@ print(problem.x_global)
 print(problem.evaluate(problem.x_global))
 print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
+
+
+## Test CEC2014 F4
+print("====================F4")
+problem = opfunu.cec_based.F42014(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
