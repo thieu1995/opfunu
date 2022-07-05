cff439512c940005c0a96908640290616ceaa6ca
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-05 16:59:11 +0700

    Add CEC-2014 F27

--- examples/cec_based/cec2014.py
+++ examples/cec_based/cec2014.py
@@ -321,6 +321,17 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2014 F27
+print("====================F27")
+problem = opfunu.cec_based.F272014(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
 
 
 
