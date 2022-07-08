7d0d2bfbcfb397c7c795d25d6a788d2047b5775e
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-08 20:42:20 +0700

    Add CEC-2017 F18

--- tests/cec_based/test_cec2017.py
+++ tests/cec_based/test_cec2017.py
@@ -230,6 +230,18 @@ def test_F172017_results():
     assert len(problem.x_global) == ndim
 
 
+def test_F182017_results():
+    ndim = 30
+    problem = opfunu.cec_based.F182017(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
+
 
 
 
