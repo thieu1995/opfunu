6d8a323ed34cfd76403a23e907fe3909e92f44bc
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-08 18:28:30 +0700

    Add CEC-2017 F8

--- examples/cec_based/cec2017.py
+++ examples/cec_based/cec2017.py
@@ -92,6 +92,18 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2017 F8
+print("====================F8")
+problem = opfunu.cec_based.F82017(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
 
 
 
--- tests/cec_based/test_cec2017.py
+++ tests/cec_based/test_cec2017.py
@@ -100,7 +100,17 @@ def test_F72017_results():
     assert len(problem.x_global) == ndim
 
 
-
+def test_F82017_results():
+    ndim = 30
+    problem = opfunu.cec_based.F82017(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
 
 
 
