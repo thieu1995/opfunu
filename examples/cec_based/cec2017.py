0e4bc7301d48f763d0e15c278d8cb3c4d332c680
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-08 20:29:08 +0700

    Add CEC-2017 F14

--- examples/cec_based/cec2017.py
+++ examples/cec_based/cec2017.py
@@ -164,6 +164,17 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2017 F14
+print("====================F14")
+problem = opfunu.cec_based.F142017(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
 
 
 
--- opfunu/cec_based/cec2017.py
+++ opfunu/cec_based/cec2017.py
@@ -435,6 +435,37 @@ class F132017(F102017):
         return self.g1(mz[self.idx1]) + self.g2(mz[self.idx2]) + self.g3(mz[self.idx3]) + self.g4(mz[self.idx4]) + self.f_bias
 
 
+class F142017(F102017):
+    """
+    .. [1] Problem Definitions and Evaluation Criteria for the CEC 2017
+    Special Session and Competition on Single Objective Real-Parameter Numerical Optimization
+    """
+    name = "F14: Hybrid Function 5"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 1400.0'
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data_14", f_matrix="M_14_D", f_shuffle="shuffle_data_14_D", f_bias=1400.):
+        super().__init__(ndim, bounds, f_shift, f_matrix, f_shuffle, f_bias)
+        self.n_funcs = 4
+        self.p = np.array([0.2, 0.2, 0.3, 0.3])
+        self.n1 = int(np.ceil(self.p[0] * self.ndim))
+        self.n2 = int(np.ceil(self.p[1] * self.ndim)) + self.n1
+        self.n3 = int(np.ceil(self.p[2] * self.ndim)) + self.n2
+        self.idx1, self.idx2 = self.f_shuffle[:self.n1], self.f_shuffle[self.n1:self.n2]
+        self.idx3, self.idx4 = self.f_shuffle[self.n2:self.n3], self.f_shuffle[self.n3:self.ndim]
+        self.g1 = operator.bent_cigar_func
+        self.g2 = operator.hgbat_func
+        self.g3 = operator.rastrigin_func
+        self.g4 = operator.rosenbrock_func
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias, "f_matrix": self.f_matrix, "f_shuffle": self.f_shuffle}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+        mz = np.dot(self.f_matrix, x - self.f_shift)
+        return self.g1(mz[self.idx1]) + self.g2(mz[self.idx2]) + self.g3(mz[self.idx3]) + self.g4(mz[self.idx4]) + self.f_bias
 
 
 
