ccb5b35c1261e703b3212f69a5080058ac6cab1d
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-08 20:33:24 +0700

    Add CEC-2017 F15

--- examples/cec_based/cec2017.py
+++ examples/cec_based/cec2017.py
@@ -176,6 +176,17 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2017 F15
+print("====================F15")
+problem = opfunu.cec_based.F152017(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
 
 
 
