9c4c9e5004d45cb92792609e12f88f7b0deaae7e
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-12 16:38:34 +0700

    Add support data for CEC-2019

---
+++ examples/cec_based/cec2019.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 16:37, 12/07/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import opfunu
+import numpy as np
+
+
+## Test CEC2019 F1
+print("====================F1")
+problem = opfunu.cec_based.F12019(ndim=50)
+x = np.ones(50)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
---
+++ tests/cec_based/test_cec2019.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 16:37, 12/07/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+import opfunu
+import pytest
+
+
+def test_F12017_results():
+    ndim = 30
+    problem = opfunu.cec_based.F12017(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
+
