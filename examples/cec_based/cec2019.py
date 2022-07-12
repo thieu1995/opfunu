e520c2e49e42ec2b05442f7eb13fa9134d174038
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-12 21:01:33 +0700

    Add CEC-2019 F5

--- examples/cec_based/cec2019.py
+++ examples/cec_based/cec2019.py
@@ -56,6 +56,21 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2019 F5
+print("====================F5")
+problem = opfunu.cec_based.F52019(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+
+
+
 
 
 
