931b793f83bbdf61dd05d2f1efab85e65add3e33
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-12 21:33:46 +0700

    Add support data for CEC-2020

---
+++ tests/cec_based/test_cec2020.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 21:33, 12/07/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
