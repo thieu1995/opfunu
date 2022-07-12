a3482586c15ed0bda06e7ef733cd1dbf98108cba
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-12 21:47:04 +0700

    Add CEC-2020 F1

--- tests/cec_based/test_cec2020.py
+++ tests/cec_based/test_cec2020.py
@@ -3,3 +3,26 @@
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
+
+import numpy as np
+import opfunu
+import pytest
+
+
+def test_F12020_results():
+    ndim = 30
+    problem = opfunu.cec_based.F12020(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
+
+
+
+
+
+
