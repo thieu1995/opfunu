13275600f6d027825b237826d2d079f414f4df07
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 11:00:48 +0700

    Add CEC-2022 F12

--- examples/cec_based/cec2022.py
+++ examples/cec_based/cec2022.py
@@ -140,5 +140,13 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2022 F12
+print("====================F12")
+problem = opfunu.cec_based.F122022(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
 
-
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
--- tests/cec_based/test_cec2022.py
+++ tests/cec_based/test_cec2022.py
@@ -152,5 +152,14 @@ def test_F112022_results():
     assert len(problem.x_global) == ndim
 
 
-
-
+def test_F122022_results():
+    ndim = 10
+    problem = opfunu.cec_based.F122022(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
