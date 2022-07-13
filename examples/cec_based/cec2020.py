ee95f892418a92d1381555712edfa43d3f797326
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 07:39:00 +0700

    Add CEC-2020 F7

--- examples/cec_based/cec2020.py
+++ examples/cec_based/cec2020.py
@@ -80,5 +80,18 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2020 F7
+print("====================F7")
+problem = opfunu.cec_based.F72020(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+
 
 
--- tests/cec_based/test_cec2020.py
+++ tests/cec_based/test_cec2020.py
@@ -87,4 +87,18 @@ def test_F62020_results():
     assert len(problem.x_global) == ndim
 
 
+def test_F72020_results():
+    ndim = 30
+    problem = opfunu.cec_based.F72020(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
+
+
+
 
