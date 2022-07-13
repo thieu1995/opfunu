8833071bc7dcb8a30e724dabc1d1919ac785c3cf
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 10:39:44 +0700

    Add CEC-2022 F9

--- tests/cec_based/test_cec2022.py
+++ tests/cec_based/test_cec2022.py
@@ -113,4 +113,18 @@ def test_F82022_results():
     assert len(problem.x_global) == ndim
 
 
+def test_F92022_results():
+    ndim = 10
+    problem = opfunu.cec_based.F92022(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
+
+
+
 
