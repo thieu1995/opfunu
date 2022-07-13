a2d4e793757ce1ea7b022d2b3da7c746e4010168
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 09:30:58 +0700

    Add CEC-2021 F1

--- examples/cec_based/cec2021.py
+++ examples/cec_based/cec2021.py
@@ -3,3 +3,21 @@
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
+
+import opfunu
+import numpy as np
+
+
+## Test CEC2021 F1
+print("====================F1")
+problem = opfunu.cec_based.F12021(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+
