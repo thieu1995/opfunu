51a408c8aea53de839e6c90e8930ce95acf755a4
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 07:34:44 +0700

    Add CEC-2020 F6

--- examples/cec_based/cec2020.py
+++ examples/cec_based/cec2020.py
@@ -68,6 +68,16 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2020 F6
+print("====================F6")
+problem = opfunu.cec_based.F62020(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
 
 
 
--- opfunu/cec_based/cec2020.py
+++ opfunu/cec_based/cec2020.py
@@ -219,9 +219,8 @@ class F42020(CecBenchmark):
 
 class F52020(CecBenchmark):
     """
-    .. [1] Liang, J. J., Qu, B. Y., & Suganthan, P. N. (2013). Problem definitions and evaluation criteria for the CEC 2014
-    special session and competition on single objective real-parameter numerical optimization. Computational Intelligence Laboratory,
-    Zhengzhou University, Zhengzhou China and Technical Report, Nanyang Technological University, Singapore, 635, 490.
+    .. [1] Problem Definitions and Evaluation Criteria for the CEC 2020
+    Special Session and Competition on Single Objective Bound Constrained Numerical Optimization
     """
     name = "F17: Hybrid Function 1 (F17 CEC-2014)"
     latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
@@ -280,5 +279,54 @@ class F52020(CecBenchmark):
         z1 = np.concatenate((z[self.idx1], z[self.idx2], z[self.idx3]))
         mz = np.dot(self.f_matrix, z1)
         return self.g1(mz[:self.n1]) + self.g2(mz[self.n1:self.n2]) + self.g3(mz[self.n2:]) + self.f_bias
+
+
+class F62020(CecBenchmark):
+    """
+    .. [1] Problem Definitions and Evaluation Criteria for the CEC 2020
+    Special Session and Competition on Single Objective Bound Constrained Numerical Optimization
+    """
+    name = "F16: Hybrid Function 2 (F15 CEC-2017)"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 1600.0'
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data_7", f_matrix="M_7_D", f_shuffle="shuffle_data_7_D", f_bias=1600.):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 30
+        self.dim_max = 100
+        self.dim_supported = [10, 15, 20, 30, 50, 100]
+        self.check_ndim_and_bounds(ndim, self.dim_max, bounds, np.array([[-100., 100.] for _ in range(self.dim_default)]))
+        self.make_support_data_path("data_2020")
+        self.f_shift = self.check_shift_data(f_shift)[:self.ndim]
+        self.f_matrix = self.check_matrix_data(f_matrix, needed_dim=True)
+        self.f_shuffle = self.check_shuffle_data(f_shuffle, needed_dim=True)
+        self.f_shuffle = (self.f_shuffle - 1).astype(int)
+        self.f_bias = f_bias
+        self.f_global = f_bias
+        self.x_global = self.f_shift
+        self.n_funcs = 4
+        self.p = np.array([0.2, 0.2, 0.3, 0.3])
+        self.n1 = int(np.ceil(self.p[0] * self.ndim))
+        self.n2 = int(np.ceil(self.p[1] * self.ndim)) + self.n1
+        self.n3 = int(np.ceil(self.p[2] * self.ndim)) + self.n2
+        self.idx1, self.idx2 = self.f_shuffle[:self.n1], self.f_shuffle[self.n1:self.n2]
+        self.idx3, self.idx4 = self.f_shuffle[self.n2:self.n3], self.f_shuffle[self.n3:self.ndim]
+        self.g1 = operator.expanded_scaffer_f6_func
+        self.g2 = operator.hgbat_func
+        self.g3 = operator.rosenbrock_func
+        self.g4 = operator.modified_schwefel_func
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias, "f_matrix": self.f_matrix, "f_shuffle": self.f_shuffle}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+        mz = np.dot(self.f_matrix, x - self.f_shift)
+        return self.g1(mz[self.idx1]) + self.g2(mz[self.idx2]) + self.g3(mz[self.idx3]) + self.g4(mz[self.idx4]) + self.f_bias
+
+
+
 
 
