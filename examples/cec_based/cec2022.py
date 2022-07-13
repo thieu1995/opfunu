1c3d1337f65f80dc63c12e60bcc437749ff22a7b
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 10:08:53 +0700

    Add CEC-2022 F1

--- examples/cec_based/cec2022.py
+++ examples/cec_based/cec2022.py
@@ -3,3 +3,22 @@
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
+
+import opfunu
+import numpy as np
+
+
+## Test CEC2022 F1
+print("====================F1")
+problem = opfunu.cec_based.F12022(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
+
+
--- tests/cec_based/test_cec2022.py
+++ tests/cec_based/test_cec2022.py
@@ -3,3 +3,29 @@
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
+
+import numpy as np
+import opfunu
+import pytest
+
+
+def test_F12022_results():
+    ndim = 10
+    problem = opfunu.cec_based.F12022(ndim=ndim)
+    x = np.ones(ndim)
+    result = problem.evaluate(x)
+    assert isinstance(problem, opfunu.cec_based.CecBenchmark)
+    assert isinstance(problem, opfunu.name_based.Benchmark)
+    assert isinstance(problem.lb, np.ndarray)
+    assert len(problem.lb) == ndim
+    assert problem.bounds.shape[0] == ndim
+    assert len(problem.x_global) == ndim
+
+
+
+
+
+
+
+
+
