40cb1adab1f1d2a4d3184d608c58408568b9c18c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 08:53:50 +0700

    Add CEC-2020 F8

--- opfunu/cec_based/cec2020.py
+++ opfunu/cec_based/cec2020.py
@@ -348,9 +348,8 @@ class F62020(CecBenchmark):
 
 class F72020(CecBenchmark):
     """
-    .. [1] Liang, J. J., Qu, B. Y., & Suganthan, P. N. (2013). Problem definitions and evaluation criteria for the CEC 2014
-    special session and competition on single objective real-parameter numerical optimization. Computational Intelligence Laboratory,
-    Zhengzhou University, Zhengzhou China and Technical Report, Nanyang Technological University, Singapore, 635, 490.
+    .. [1] Problem Definitions and Evaluation Criteria for the CEC 2020
+    Special Session and Competition on Single Objective Bound Constrained Numerical Optimization
     """
     name = "F7: Hybrid Function 3 (F21 CEC-2014)"
     latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
@@ -417,4 +416,77 @@ class F72020(CecBenchmark):
                self.g4(mz[self.n3:self.n4]) + self.g5(mz[self.n4:]) + self.f_bias
 
 
+class F82020(CecBenchmark):
+    """
+    .. [1] Problem Definitions and Evaluation Criteria for the CEC 2020
+    Special Session and Competition on Single Objective Bound Constrained Numerical Optimization
+    """
+    name = "F8: Composition Function 1 (F21 CEC-2017)"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 2200.0'
+
+    continuous = True
+    linear = False
+    convex = False
+    unimodal = False
+    separable = False
+
+    differentiable = True
+    scalable = True
+    randomized_term = False
+    parametric = True
+    shifted = True
+    rotated = True
+
+    modality = True  # Number of ambiguous peaks, unknown # peaks
+    characteristics = ["Asymmetrical", "Different properties around different local optima"]
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data_22", f_matrix="M_22_D", f_bias=2200.):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 30
+        self.dim_max = 100
+        self.dim_supported = [2, 5, 10, 15, 20, 30, 50, 100]
+        self.check_ndim_and_bounds(ndim, self.dim_max, bounds, np.array([[-100., 100.] for _ in range(self.dim_default)]))
+        self.make_support_data_path("data_2020")
+        self.f_shift = self.check_shift_matrix(f_shift)[:, :self.ndim]
+        self.f_matrix = self.check_matrix_data(f_matrix)[:, :self.ndim]
+        self.f_bias = f_bias
+        self.f_global = f_bias
+        self.x_global = self.f_shift[0]
+        self.n_funcs = 3
+        self.xichmas = [10, 20, 30]
+        self.lamdas = [1., 10., 1.]
+        self.bias = [0, 100, 200]
+        self.g0 = operator.rastrigin_func
+        self.g1 = operator.griewank_func
+        self.g2 = operator.modified_schwefel_func
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias, "f_matrix": self.f_matrix}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+
+        # 1. Rastrigin’s Function F5’
+        z0 = np.dot(self.f_matrix[:self.ndim, :], x - self.f_shift[0])
+        g0 = self.lamdas[0] * self.g0(z0) + self.bias[0]
+        w0 = operator.calculate_weight(x - self.f_shift[0], self.xichmas[0])
+
+        # 2. Griewank’s Function F15’
+        z1 = np.dot(self.f_matrix[self.ndim:2*self.ndim, :], x - self.f_shift[1])
+        g1 = self.lamdas[1] * self.g1(z1) + self.bias[1]
+        w1 = operator.calculate_weight(x - self.f_shift[1], self.xichmas[1])
+
+        # 3. Modifed Schwefel's Function F10’
+        # z2 = np.dot(self.f_matrix[2*self.ndim:3*self.ndim, :], x - self.f_shift[2])
+        z2 = 1000*(x - self.f_shift[2])/100
+        g2 = self.lamdas[2] * self.g2(z2) + self.bias[2]
+        w2 = operator.calculate_weight(x - self.f_shift[2], self.xichmas[2])
+
+        ws = np.array([w0, w1, w2])
+        ws = ws / np.sum(ws)
+        gs = np.array([g0, g1, g2])
+        return np.dot(ws, gs) + self.f_bias
 
