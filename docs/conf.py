783b09dbfad376925c0e06a65478ea2bd642d5db
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 18:44:42 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,16 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
 import os
 import sys
-
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(1, os.path.abspath('../../opfunu'))
+
+import sphinx_rtd_theme
+import opfunu
 
 # -- Project information -----------------------------------------------------
 
