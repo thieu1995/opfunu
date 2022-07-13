b5d12e14db688aa2728a170186364d49cc823ffd
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 10:28:59 +0700

    Add CEC-2022 F7

--- examples/cec_based/cec2022.py
+++ examples/cec_based/cec2022.py
@@ -80,6 +80,18 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2022 F7
+print("====================F7")
+problem = opfunu.cec_based.F72022(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
 
 
 
