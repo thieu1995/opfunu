f7ac4191be3782bda7ab79fdb4c9cddb01868f66
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 09:34:52 +0700

    Add CEC-2021 F3

--- examples/cec_based/cec2021.py
+++ examples/cec_based/cec2021.py
@@ -32,4 +32,16 @@ print(problem.is_succeed(x))
 print(problem.is_succeed(problem.x_global))
 
 
+## Test CEC2021 F3
+print("====================F3")
+problem = opfunu.cec_based.F32021(ndim=10)
+x = np.ones(10)
+print(problem.evaluate(x))
+print(problem.x_global)
+
+print(problem.evaluate(problem.x_global))
+print(problem.is_succeed(x))
+print(problem.is_succeed(problem.x_global))
+
+
 
