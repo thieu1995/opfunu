f44f4aa5893518c4a08ce46ebe149aa68d7b628f
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-13 10:31:42 +0700

    Add CEC-2022 F8

--- opfunu/cec_based/cec2022.py
+++ opfunu/cec_based/cec2022.py
@@ -299,6 +299,75 @@ class F72022(CecBenchmark):
                self.g4(mz[self.n3:self.n4]) + self.g5(mz[self.n4:self.n5]) + self.g6(mz[self.n5:self.ndim]) + self.f_bias
 
 
+class F82022(CecBenchmark):
+    """
+    .. [1] Problem Definitions and Evaluation Criteria for the CEC 2022
+    Special Session and Competition on Single Objective Bound Constrained Numerical Optimization
+    """
+    name = "F8: Hybrid Function 3"
+    latex_formula = r'F_1(x) = \sum_{i=1}^D z_i^2 + bias, z=x-o,\\ x=[x_1, ..., x_D]; o=[o_1, ..., o_D]: \text{the shifted global optimum}'
+    latex_formula_dimension = r'2 <= D <= 100'
+    latex_formula_bounds = r'x_i \in [-100.0, 100.0], \forall i \in  [1, D]'
+    latex_formula_global_optimum = r'\text{Global optimum: } x^* = o, F_1(x^*) = bias = 2200.0'
+
+    continuous = True
+    linear = False
+    convex = False
+    unimodal = False
+    separable = False
+
+    differentiable = True
+    scalable = True
+    randomized_term = False
+    parametric = True
+    shifted = True
+    rotated = True
+
+    modality = True  # Number of ambiguous peaks, unknown # peaks
+    # n_basins = 1
+    # n_valleys = 1
+
+    characteristics = ["Different properties for different variables subcomponents"]
+
+    def __init__(self, ndim=None, bounds=None, f_shift="shift_data_8", f_matrix="M_8_D", f_shuffle="shuffle_data_8_D", f_bias=2200.):
+        super().__init__()
+        self.dim_changeable = True
+        self.dim_default = 10
+        self.dim_max = 20
+        self.dim_supported = [10, 20]
+        self.check_ndim_and_bounds(ndim, self.dim_max, bounds, np.array([[-100., 100.] for _ in range(self.dim_default)]))
+        self.make_support_data_path("data_2022")
+        self.f_shift = self.check_shift_data(f_shift)[:self.ndim]
+        self.f_matrix = self.check_matrix_data(f_matrix, needed_dim=True)
+        self.f_shuffle = self.check_shuffle_data(f_shuffle, needed_dim=True)
+        self.f_shuffle = (self.f_shuffle - 1).astype(int)
+        self.f_bias = f_bias
+        self.f_global = f_bias
+        self.x_global = self.f_shift
+        self.n_funcs = 6
+        self.p = np.array([0.3, 0.2, 0.2, 0.1, 0.2])
+        self.n1 = int(np.ceil(self.p[0] * self.ndim))
+        self.n2 = int(np.ceil(self.p[1] * self.ndim)) + self.n1
+        self.n3 = int(np.ceil(self.p[2] * self.ndim)) + self.n2
+        self.n4 = int(np.ceil(self.p[3] * self.ndim)) + self.n3
+        self.idx1, self.idx2, self.idx3 = self.f_shuffle[:self.n1], self.f_shuffle[self.n1:self.n2], self.f_shuffle[self.n2:self.n3]
+        self.idx4, self.idx5 = self.f_shuffle[self.n3:self.n4], self.f_shuffle[self.n4:self.ndim]
+        self.g1 = operator.katsuura_func
+        self.g2 = operator.happy_cat_func
+        self.g3 = operator.expanded_griewank_rosenbrock_func
+        self.g4 = operator.modified_schwefel_func
+        self.g5 = operator.ackley_func
+        self.paras = {"f_shift": self.f_shift, "f_bias": self.f_bias, "f_matrix": self.f_matrix, "f_shuffle": self.f_shuffle}
+
+    def evaluate(self, x, *args):
+        self.n_fe += 1
+        self.check_solution(x, self.dim_max, self.dim_supported)
+        z = x - self.f_shift
+        z1 = np.concatenate((z[self.idx1], z[self.idx2], z[self.idx3], z[self.idx4], z[self.idx5]))
+        mz = np.dot(self.f_matrix, z1)
+        return self.g1(mz[:self.n1]) + self.g2(mz[self.n1:self.n2]) + self.g3(mz[self.n2:self.n3]) + \
+               self.g4(mz[self.n3:self.n4]) + self.g5(mz[self.n4:self.ndim]) + self.f_bias
+
 
 
 
