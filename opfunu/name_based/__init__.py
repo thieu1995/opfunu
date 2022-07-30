b398b9628579afd5eede30ba7074d3ea8b747baa
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-07-30 17:42:38 +0700

    Update all normal function left

--- opfunu/name_based/__init__.py
+++ opfunu/name_based/__init__.py
@@ -13,5 +13,22 @@ from .f_func import *
 from .g_func import *
 from .h_func import *
 from .i_func import *
+from .j_func import *
+from .k_func import *
+from .l_func import *
+from .m_func import *
+from .n_func import *
+from .o_func import *
+from .p_func import *
+from .q_func import *
+from .r_func import *
+from .s_func import *
+from .t_func import *
+from .u_func import *
+from .v_func import *
+from .w_func import *
+from .x_func import *
+from .y_func import *
+from .z_func import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
---
+++ tests/name_based/test_x_func.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:39, 30/07/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
