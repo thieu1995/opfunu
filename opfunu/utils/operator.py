f35bc8370ce24fe7847e5f777bc724a3a247c439
Travis Silvers <firestrand@gmail.com>
2023-09-18 21:43:15 -0700

    Optimization of rounder method

--- opfunu/utils/operator.py
+++ opfunu/utils/operator.py
@@ -8,18 +8,12 @@ import numpy as np
 
 
 def rounder(x, condition):
-    ndim = len(x)
     temp_2x = 2 * x
-    for idx in range(0, ndim):
-        dec, inter = np.modf(temp_2x[idx])
-        if temp_2x[idx] <= 0 and dec >= 0.5:
-            temp_2x[idx] = inter - 1
-        elif dec < 0.5:
-            temp_2x[idx] = inter
-        else:
-            temp_2x[idx] = inter + 1
-    temp_2x = temp_2x / 2
-    return np.where(condition < 0.5, x, temp_2x)
+    dec, inter = np.modf(temp_2x)
+    temp_2x = np.where(temp_2x <= 0.0, inter - (dec >= 0.5), temp_2x)
+    temp_2x = np.where(dec < 0.5, inter, temp_2x)
+    temp_2x = np.where(dec >= 0.5, inter + 1, temp_2x)
+    return np.where(condition < 0.5, x, temp_2x / 2)
 
 
 def griewank_func(x):
